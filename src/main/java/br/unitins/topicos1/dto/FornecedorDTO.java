3874a3e0c8627cec64c76eb6099968dadcf5d42b
danielmariae <lucasdrodrigues70@gmail.com>
2023-10-07 11:14:53 -0300

    update DTO

--- src/main/java/br/unitins/topicos1/dto/FornecedorDTO.java
+++ src/main/java/br/unitins/topicos1/dto/FornecedorDTO.java
@@ -2,15 +2,11 @@ package br.unitins.topicos1.dto;
 
 import java.util.List;
 
-import br.unitins.topicos1.model.Endereco;
-import br.unitins.topicos1.model.Lote;
-import br.unitins.topicos1.model.Telefone;
-
 public record FornecedorDTO(
   String nomeFantasia,
   String nomeRegistro,
   String endSite,
-  List<Endereco> listaEndereco,
-  List<Telefone> listaTelefone,
-  List<Lote> listaLote
+  List<EnderecoDTO> listaEndereco,
+  List<TelefoneDTO> listaTelefone,
+  List<LoteDTO> listaLote
 ) {}
