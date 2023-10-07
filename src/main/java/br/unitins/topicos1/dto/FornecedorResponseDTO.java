bd7532bb44be4ce5e11a15deb1fc20b879dfe75b
danielmariae <lucasdrodrigues70@gmail.com>
2023-10-07 11:14:35 -0300

    correcting dto patterns

--- src/main/java/br/unitins/topicos1/dto/FornecedorResponseDTO.java
+++ src/main/java/br/unitins/topicos1/dto/FornecedorResponseDTO.java
@@ -2,39 +2,35 @@ package br.unitins.topicos1.dto;
 
 import java.util.List;
 
-import br.unitins.topicos1.model.Endereco;
 import br.unitins.topicos1.model.Fornecedor;
-import br.unitins.topicos1.model.Lote;
-import br.unitins.topicos1.model.Telefone;
 
 public record FornecedorResponseDTO(
   String nomeFantasia,
   String nomeRegistro,
   String endSite,
-  List<Endereco> listaEndereco,
-  List<Telefone> listaTelefone, 
-  List<Lote> listaLote
-  ) {
-
-public static FornecedorResponseDTO valueOf(Fornecedor fornecedor) {
-    return new FornecedorResponseDTO(
-      fornecedor.getNomeFantasia(),
-      fornecedor.getNomeRegistro(),
-      fornecedor.getEndSite(),
-      fornecedor
-        .getListaEndereco()
-        .stream()
-        .map(e -> Endereco.valueOf(e))
-        .toList(),
-      fornecedor
-        .getListaTelefone()
-        .stream()
-        .map(t -> Telefone.valueOf(t))
-        .toList(),
-      fornecedor
-        .getListaLote()
-        .stream()
-        .map(l -> Lote.valueOf(l))
-        .toList());
+  List<EnderecoResponseDTO> listaEndereco,
+  List<TelefoneResponseDTO> listaTelefone, 
+  List<LoteResponseDTO> listaLote) {
+  public static FornecedorResponseDTO valueOf(Fornecedor fornecedor) {
+      return new FornecedorResponseDTO(
+        fornecedor.getNomeFantasia(),
+        fornecedor.getNomeRegistro(),
+        fornecedor.getEndSite(),
+        fornecedor
+          .getListaEndereco()
+          .stream()
+          .map(e -> EnderecoResponseDTO.valueOf(e))
+          .toList(),
+        fornecedor
+          .getListaTelefone()
+          .stream()
+          .map(t -> TelefoneResponseDTO.valueOf(t))
+          .toList(),
+        fornecedor
+          .getListaLote()
+          .stream()
+          .map(l -> LoteResponseDTO.valueOf(l))
+          .toList()
+        );
   }
 }
