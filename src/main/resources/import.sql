9b1d1b0ae3d2329a4a874de30dc97a278a193eb7
leocid25 <lggarcia@uft.edu.br>
2023-11-09 09:50:09 -0300

    tersfeste

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -91,7 +91,7 @@ insert into itemDaVenda (preco, quantidade, produto_id) values(1000.0, 2, 3);
 
 insert into formaDePagamento (forma_de_pagamento, modalidade, valorPago) values('CARTAO_DE_CREDITO',0, 5900);
 insert into cartaodecredito (id, anovalidade, codseguranca, mesvalidade, datahorapagamento, numerocartao) values(1, 24, 237, 3, '2023-10-01 16:11:26', '345690872106');
-insert into pedido (codigoDeRastreamento, id_formaDePagamento, id_endereco) values('Der34ewww', 1, 1);
+insert into pedido (id_formaDePagamento, id_endereco) values(1, 1);
 
 insert into cliente_pedido (id_cliente, id_pedido) values(1,1);
 insert into produto_lote (id_produto, id_lote) values(1,1);
