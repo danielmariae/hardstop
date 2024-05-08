aaba58b28acb8079e15dea633894725445f7b9f6
danielmariae <lucasdrodrigues70@gmail.com>
2024-05-08 09:37:32 -0300

    updates on login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SessionTokenService } from './services/session-token.service';
+import { CommonModule } from '@angular/common';
+import { NavigationService } from './services/navigation.service';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   title = 'frontend';
+  usuarioLogado = false;
+  constructor(
+    private sessionTokenService: SessionTokenService,
+    private navigationService: NavigationService,
+  ){}
 
   ngOnInit(): void {
     // Get all "navbar-burger" elements
@@ -33,6 +41,21 @@ export class AppComponent implements OnInit{
         }
       });
     });
+
+    const headers = this.sessionTokenService.getSessionHeader();
+    
+    if(headers)
+      this.usuarioLogado = true;
+    else
+      this.usuarioLogado = false;
+
+      console.log(this.usuarioLogado);
+  }
+
+  deslogarUsuario(): void {
+    this.sessionTokenService.clearSessionToken();
+    this.navigationService.navigateTo('/home');
+    
   }
 }
 
