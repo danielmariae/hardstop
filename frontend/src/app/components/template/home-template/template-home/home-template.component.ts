400d5bd35f353e37eea6ecba35b61b1a97029ee1
danielmariae <lucasdrodrigues70@gmail.com>
2024-06-03 10:46:36 -0300

    updates on home template

--- frontend/src/app/components/template/home-template/template-home/home-template.component.ts
+++ frontend/src/app/components/template/home-template/template-home/home-template.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { HeaderHomeComponent } from "../header-home/header-home.component";
 import { CommonModule, NgFor } from '@angular/common';
 import { FooterHomeComponent } from "../footer-home/footer-home.component";
@@ -12,6 +12,9 @@ import { CarrinhoService } from '../../../../services/carrinho.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ConsultaService } from '../../../../services/consulta.service';
 import { ClienteService } from '../../../../services/cliente.service';
+import { ProdutoService } from '../../../../services/produto.service';
+import { PageEvent } from '@angular/material/paginator';
+import { Produto } from '../../../../models/produto.model';
 
 
 
@@ -34,14 +37,25 @@ type Card = {
 export class HomeTemplateComponent implements OnInit{
     cards = signal<Card[]> ([]);
     consultas: Consulta[] = [];
+    produtos: Produto[] = [];
+    totalRecords = 0;
+    page = 0;
+    pageSize = 0;
+    totalPages = 0;
+    imagensBase64: { [produtoId: number]: string } = {};
 
     constructor(private consultaService: ConsultaService, 
+        private produtoService: ProdutoService,
         private carrinhoService: CarrinhoService,
         private snackBar: MatSnackBar,
-        private clienteService: ClienteService) {}
+        private clienteService: ClienteService,
+      ) {}
 
 ngOnInit(): void {
-this.carregarConsultas();
+  this.pageSize = 2; 
+  this.page=0;
+  this.carregarProdutos(this.page, this.pageSize);
+  console.log(this.produtos);
 }
 
 carregarConsultas() {
@@ -50,6 +64,44 @@ carregarConsultas() {
       this.consultas = data;
       // console.log(data);
       this.carregarCards();
+    });
+  }
+
+  carregarProdutos(page: number, pageSize: number): void {
+    this.produtoService.findAll(this.page, this.pageSize).subscribe({
+        next: (response) => {
+            console.log(response);
+            this.produtos = response;
+            this.totalRecords = 
+            this.totalPages = Math.round(this.totalRecords/this.pageSize);
+            this.carregarImagensParaProdutos();
+            this.carregarCards();
+        },
+        error: (error) => {
+            // Este callback é executado quando ocorre um erro durante a emissão do valor
+            console.error('Erro:', error);
+            window.alert(error);
+        } 
+    })
+  }
+
+  carregarImagensParaProdutos(): void {
+    this.produtos.forEach(produto => {
+      if (produto.imagemPrincipal) {
+        this.produtoService.getImageAsBase64(produto.imagemPrincipal)
+          .then(imagemBase64 => {
+            if (produto.id !== undefined) {
+              this.imagensBase64[produto.id] = imagemBase64;
+            }
+          })
+          .catch(error => {
+            console.error(`Erro ao carregar imagem para o produto ${produto.id}:`, error);
+          });
+      } else {
+        if (produto.id !== undefined) {
+          this.imagensBase64[produto.id] = '';
+        }
+      }
     });
   }
 
@@ -100,6 +152,23 @@ carregarConsultas() {
       horizontalPosition: "center" // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
     });
   }
+    // Método para paginar os resultados
+paginar(event: PageEvent) : void {
+  this.page = event.pageIndex;
+  this.pageSize = event.pageSize;
+  this.atualizarDadosDaPagina();
+}
+
+onChange(event:any): void{
+  const value = event.target.value;
+  console.log(value);
+  this.paginar({ pageIndex: 0, pageSize: parseInt(value), length: this.totalPages }); 
+}
+
+  // Método para paginar os resultados
+  atualizarDadosDaPagina(): void {
+    this.carregarProdutos(this.page, this.pageSize);
+}
 
 
 
