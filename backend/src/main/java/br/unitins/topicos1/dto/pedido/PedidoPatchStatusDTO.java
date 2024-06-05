715a43ab42791d461a179a24503441e3380c6d61
leocid25 <lggarcia@uft.edu.br>
2024-06-05 12:35:43 -0300

    mudanca

--- backend/src/main/java/br/unitins/topicos1/dto/pedido/PedidoPatchStatusDTO.java
+++ backend/src/main/java/br/unitins/topicos1/dto/pedido/PedidoPatchStatusDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 public record PedidoPatchStatusDTO(
 
     String codigoDeRastreamento,
+    Integer idTransportadora,
     @NotNull(message = "O campo Id do Pedido não pode ser nulo")
     Long idPedido,
     @NotNull(message = "O campo Id do status não pode ser nulo")
